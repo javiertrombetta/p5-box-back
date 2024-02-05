230f33fe076084baf880c0c850bcd4a07f826172
javiertrombetta <jitrombetta@hotmail.com>
2024-02-05 08:04:40 -0300

    feat(common): add common folders

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { UsersModule } from './users/users.module';
 import { SeedModule } from './seed/seed.module';
 import { PackagesModule } from './packages/packages.module';
 import { ReportsModule } from './reports/reports.module';
 import { RewardsModule } from './rewards/rewards.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from './common/common.module';
+
 
 @Module({
 	imports: [
@@ -19,6 +22,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 		PackagesModule,
 		ReportsModule,
 		RewardsModule,
+		CommonModule,
 	],
 	controllers: [],
 	providers: [],
