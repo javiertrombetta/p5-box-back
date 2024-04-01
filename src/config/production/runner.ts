940c144bc88bae106a943ad4714dfecb659b704b
javiertrombetta <jitrombetta@hotmail.com>
2024-04-01 14:05:07 -0300

    fix : change config runner for production

--- src/config/production/runner.ts
+++ src/config/production/runner.ts
@@ -1,7 +1,7 @@
 import { execSync } from 'child_process';
 
 console.log('Ejecutando el proceso de seed con datos de ejemplo...');
-execSync('node dist/examples.js', { stdio: 'inherit' });
+execSync('node dist/config/production/examples.js', { stdio: 'inherit' });
 
 console.log('Iniciando la aplicación...');
 execSync('node dist/main.js', { stdio: 'inherit' });
